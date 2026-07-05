df04a2d568cb167999bbb33ccf23ffd41fbcfa50
Lucca Sena <161384749+luccasena@users.noreply.github.com>
2026-07-05 16:38:43 -0300

    feature: Validação nos DTOs de Livro.

--- back_end/src/main/java/io/github/bookstore/back_end/dto/book/BookCreateDTO.java
+++ back_end/src/main/java/io/github/bookstore/back_end/dto/book/BookCreateDTO.java
@@ -7,14 +7,24 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public record BookCreateDTO(
+     @Size(min = 13,max = 13, message = "Deve ter 13 caracteres.")
+     @NotBlank(message = "Não pode ser vazio.")
      String isbn,
+     @NotBlank(message = "Não pode ser vazio.")
      String title,
+     @NotBlank(message = "Não pode ser vazio.")
      String description,
+     @NotBlank(message = "Não pode ser vazio.")
      String image,
+     @NotBlank(message = "Não pode ser vazio.")
      String publisherCnpj,
+     @NotNull(message = "Não pode ser nulo.")
      int stockQuantity,
+     @NotNull(message = "Não pode ser nulo.")
      double price,
      double review,
+     @NotNull(message = "Não pode ser nulo.")
      Language language,
+     @NotNull(message = "Não pode ser nulo.")
      int pageCount
 ) {}
--- back_end/src/main/java/io/github/bookstore/back_end/dto/book/BookUpdateDTO.java
+++ back_end/src/main/java/io/github/bookstore/back_end/dto/book/BookUpdateDTO.java
@@ -1,16 +1,29 @@
 package io.github.bookstore.back_end.dto.book;
 
 import io.github.bookstore.back_end.models.enums.Language;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 public record BookUpdateDTO(
+        @Size(min = 13,max = 13, message = "Deve ter 13 caracteres.")
+        @NotBlank(message = "Não pode ser vazio.")
         String isbn,
+        @NotBlank(message = "Não pode ser vazio.")
         String title,
+        @NotBlank(message = "Não pode ser vazio.")
         String description,
+        @NotBlank(message = "Não pode ser vazio.")
         String image,
+        @NotBlank(message = "Não pode ser vazio.")
         String publisherCnpj,
-        Integer stockQuantity,
-        Double price,
-        Double review,
+        @NotNull(message = "Não pode ser nulo.")
+        int stockQuantity,
+        @NotNull(message = "Não pode ser nulo.")
+        double price,
+        double review,
+        @NotNull(message = "Não pode ser nulo.")
         Language language,
-        Integer pageCount
+        @NotNull(message = "Não pode ser nulo.")
+        int pageCount
 ) {}
