b54c01ae906f5a63dfcf06fec01b96b6dde9581d
Lucca Sena <161384749+luccasena@users.noreply.github.com>
2026-07-05 16:34:11 -0300

    feature: book controller

---
+++ back_end/src/main/java/io/github/bookstore/back_end/controllers/BookController.java
@@ -0,0 +1,38 @@
+package io.github.bookstore.back_end.controllers;
+
+import io.github.bookstore.back_end.dto.book.BookCreateDTO;
+import io.github.bookstore.back_end.dto.book.BookUpdateDTO;
+import io.github.bookstore.back_end.services.BookService;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RequestMapping("/books")
+@RestController
+@AllArgsConstructor
+public class BookController {
+
+    private final BookService bookService;
+
+    @GetMapping("/{isbn}")
+    public ResponseEntity<?> getBookIsbn(@Valid @PathVariable String isbn) {
+        return bookService.getBookByIsbn(isbn);
+    }
+
+    @PostMapping
+    public ResponseEntity<?> createBook(@Valid @RequestBody BookCreateDTO bookCreateDTO) {
+        return bookService.createBook(bookCreateDTO);
+    }
+
+    @PutMapping
+    public ResponseEntity<?> updateBook(@Valid @RequestBody BookUpdateDTO bookUpdateDTO) {
+        return bookService.updateBook(bookUpdateDTO);
+    }
+
+    @DeleteMapping("/{isbn}")
+    public ResponseEntity<?> deleteBookByIsbn(@Valid @PathVariable String isbn) {
+        return bookService.deleteBook(isbn);
+    }
+
+}
